7cc65c864d5385eeecf2b2968fd548bbfaff36ab
ty21t <tylerhutch21@gmail.com>
2013-12-08 16:46:32 -0600

    Forgot to add files when Michael distracted me by his monkey face.

---
+++ RemoveUserAss.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SCPDb
+{
+    partial class RemoveUserAss
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.comboUser = new System.Windows.Forms.ComboBox();
+            this.labelItem = new System.Windows.Forms.Label();
+            this.comboItem = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonCancel.Location = new System.Drawing.Point(274, 272);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(208, 52);
+            this.buttonCancel.TabIndex = 17;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonSave.Location = new System.Drawing.Point(42, 272);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(208, 52);
+            this.buttonSave.TabIndex = 16;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelUser.Location = new System.Drawing.Point(89, 18);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(59, 25);
+            this.labelUser.TabIndex = 15;
+            this.labelUser.Text = "User:";
+            // 
+            // comboUser
+            // 
+            this.comboUser.FormattingEnabled = true;
+            this.comboUser.Location = new System.Drawing.Point(42, 46);
+            this.comboUser.Name = "comboUser";
+            this.comboUser.Size = new System.Drawing.Size(190, 21);
+            this.comboUser.TabIndex = 14;
+            // 
+            // labelItem
+            // 
+            this.labelItem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelItem.AutoSize = true;
+            this.labelItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelItem.Location = new System.Drawing.Point(321, 18);
+            this.labelItem.Name = "labelItem";
+            this.labelItem.Size = new System.Drawing.Size(102, 25);
+            this.labelItem.TabIndex = 13;
+            this.labelItem.Text = "Item SCP:";
+            // 
+            // comboItem
+            // 
+            this.comboItem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboItem.FormattingEnabled = true;
+            this.comboItem.Location = new System.Drawing.Point(274, 46);
+            this.comboItem.Name = "comboItem";
+            this.comboItem.Size = new System.Drawing.Size(190, 21);
+            this.comboItem.TabIndex = 12;
+            // 
+            // RemoveUserAss
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(24)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(549, 384);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.comboUser);
+            this.Controls.Add(this.labelItem);
+            this.Controls.Add(this.comboItem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "RemoveUserAss";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RemoveUserAss";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.ComboBox comboUser;
+        private System.Windows.Forms.Label labelItem;
+        private System.Windows.Forms.ComboBox comboItem;
+    }
+}
